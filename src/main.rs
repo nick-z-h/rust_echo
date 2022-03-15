06cde9cb33dab76b95f46f7a7e922fa1e74ceb33
n <n.zhanghoule@gmail.com>
2022-03-15 14:54:24 -0400

    feat: implement basic functionality and -n option

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Cli {
+    #[clap(help = "Input", min_values = 1, required = true)]
+    text: Vec<String>,
+
+    #[clap(short, help = "Do not output the trailing newline")]
+    n: bool,
+}
+fn main() {
+    let cli = Cli::parse();
+    print!("{}{}", cli.text.join(" "), if cli.n { "" } else { "\n" });
+}
